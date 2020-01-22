2b51df34f4ea705a9df6c720bf58f653d519bae6
Benas Milasauskas <benas@zenitech.co.uk>
2020-01-22 20:09:16 +0200

    First commit

---
+++ src/Indai.java
@@ -0,0 +1,17 @@
+public class Indai {
+
+    private String str;
+
+    private int aukstis;
+
+    private int skyscioKiekis;
+
+    public Indai(int aukstis, int skyscioKiekis){
+        aukstis = aukstis;
+    }
+
+
+
+
+
+}
